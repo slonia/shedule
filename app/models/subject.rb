73041dd42caca03d7497b4951494d2264da38816
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2013-02-03 16:24:13 +0300

    cleaned models

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,7 +1,6 @@
  # coding: utf-8
 class Subject < ActiveRecord::Base
 	belongs_to :group
-	validates :day_id, presence: true
 	validates :name, presence: true
 	S_TYPES=["Лекция","Практика","Семинар","Лабораторная работа","Другое"]
 end
