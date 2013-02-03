e5b1e9ac74cdb692510cede36b61315825b6c1d7
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2013-02-03 16:26:54 +0300

    cleaned models 2

--- app/models/day.rb
+++ app/models/day.rb
@@ -4,7 +4,6 @@ class Day < ActiveRecord::Base
 	has_many :subjects, :order =>"num", dependent: :destroy
 	before_save :ensure_days_amount
 	DAY_NAMES = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"]
-	validates :group_id, presence: true
 	accepts_nested_attributes_for :subjects, allow_destroy: true
 
 	private
