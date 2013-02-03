4e6af9f67782a185df579e80d85a6dde967035fb
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2013-02-03 16:05:36 +0300

    cleaned groups

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ActiveRecord::Base
 	has_many :groups, :order =>"name", dependent: :destroy
 	belongs_to :faculty
 	validates :num, presence: true
+	validates :faculty_id, presence: true
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,7 @@
  # coding: utf-8
 class Subject < ActiveRecord::Base
 	belongs_to :group
+	validates :day_id, presence: true
+	validates :name, presence: true
 	S_TYPES=["Лекция","Практика","Семинар","Лабораторная работа","Другое"]
 end
